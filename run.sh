30161af2f49ebb83d21ae4f0b13992779b3e0a70
Yousaf Nabi <yousafn@gmail.com>
2025-09-08 19:05:53 +0100

    test: update commands after broker testing / impl
    run with
    
    BIN='cargo run --' ./run.sh
    BIN='pact-broker' ./run.sh
    
    to test against original pact_broker-client and pact-broker-cli

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ ${BIN} pact-broker create-environment --name name_foo1
 ${BIN} pact-broker create-environment --name name_foo2 --display-name display_name_foo
 ${BIN} pact-broker create-environment --name name_foo3 --display-name display_name_foo --contact-name contact_name_foo
 ${BIN} pact-broker create-environment --name name_foo4 --display-name display_name_foo --contact-name contact_name_foo --contact-email-address contact.email.address@foo.bar
-export UUID=$(${BIN} pact-broker create-environment --name name_foo5 --output=id)
-${BIN} pact-broker describe-environment --uuid $UUID
-${BIN} pact-broker update-environment --uuid $UUID --name name_foo6
-${BIN} pact-broker update-environment --uuid $UUID --name name_foo7 --display-name display_name_foo6
-${BIN} pact-broker update-environment --uuid $UUID --name name_foo8 --contact-name contact_name_foo8
-${BIN} pact-broker update-environment --uuid $UUID --name name_foo9 --contact-name contact_name_foo9 --contact-email-address contact_name_foo7
-${BIN} pact-broker delete-environment --uuid $UUID
+export ENV_UUID=$(${BIN} pact-broker create-environment --name name_foo5 --output=id)
+${BIN} pact-broker describe-environment --uuid $ENV_UUID
+${BIN} pact-broker update-environment --uuid $ENV_UUID --name name_foo6
+${BIN} pact-broker update-environment --uuid $ENV_UUID --name name_foo7 --display-name display_name_foo6
+${BIN} pact-broker update-environment --uuid $ENV_UUID --name name_foo8 --contact-name contact_name_foo8
+${BIN} pact-broker update-environment --uuid $ENV_UUID --name name_foo9 --contact-name contact_name_foo9 --contact-email-address contact_name_foo7
+${BIN} pact-broker delete-environment --uuid $ENV_UUID
 ${BIN} pact-broker list-environments | awk -F '│' '{print $2}' | sed -n '3,$p' | sed '$d' | awk '{print $1}' | xargs -I {} ${BIN} pact-broker delete-environment --uuid {} 
 ${BIN} pact-broker create-environment --name production --production
 ${BIN} pact-broker publish --dir pacts -r
@@ -28,16 +28,18 @@ ${BIN} pact-broker record-deployment --version foo --environment production --pa
 ${BIN} pact-broker record-undeployment --environment production --pacticipant GettingStartedOrderWeb
 ${BIN} pact-broker record-release --version foo --environment production --pacticipant GettingStartedOrderWeb
 ${BIN} pact-broker record-support-ended --version foo --environment production --pacticipant GettingStartedOrderWeb
-${BIN} pact-broker can-i-merge --pacticipant foo
-${BIN} pact-broker create-or-update-pacticipant --name foo --main-branch bar --repository-url http://foo.bar
+${BIN} pact-broker create-or-update-pacticipant --name foo --main-branch main --repository-url http://foo.bar
 ${BIN} pact-broker describe-pacticipant --name foo
 ${BIN} pact-broker list-pacticipants
 ${BIN} pact-broker create-webhook https://localhost --request POST --contract-published
-${BIN} pact-broker create-or-update-webhook http//foo.bar --uuid bar --help
-${BIN} pact-broker test-webhook --uuid bar || echo "test-webhook unimplemented"
-${BIN} pact-broker delete-branch --branch foo --pacticipant foo
+export WEBHOOK_UUID=$(${BIN} pact-broker create-webhook https://localhost --request POST --contract-published | jq .uuid -r)
+${BIN} pact-broker create-or-update-webhook https://foo.bar --request POST --uuid $WEBHOOK_UUID --provider-verification-succeeded
+${BIN} pact-broker test-webhook --uuid $WEBHOOK_UUID
 ${BIN} pact-broker create-version-tag --version foo --pacticipant foo --tag bar
 ${BIN} pact-broker describe-version --pacticipant foo
-${BIN} pact-broker create-or-update-version --version foo --pacticipant foo --branch bar --tags baz
+${BIN} pact-broker create-or-update-version --version foo --pacticipant foo --branch bar --tag baz
+${BIN} pact-broker can-i-merge --pacticipant foo --version foo
+${BIN} pact-broker delete-branch --branch bar --pacticipant foo
+${BIN} pact-broker describe-pacticipant --name foo
 ${BIN} pact-broker generate-uuid
 
